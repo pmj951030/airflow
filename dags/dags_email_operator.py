1ffe61dc1dea26f8c299d255d1c73da2bdc7df4a
pmj951030 <audwls7857@naver.com>
2023-08-08 21:22:03 +0900

    이메일 오퍼레이터

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -9,9 +9,7 @@ with DAG(
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False, ## 날짜 누락된 구간은 코드 실행x(start_date부터 어제까지의 구간은 코드실행X)
-    # dagrun_timeout=datetime.timedelta(minutes=60),
-    tags=["example", "example2"], ## airflow에 들어왔을때 태그
-    # params={"example_key": "example_value"}, ## 덱 밑에 테스크에 공통적으로 줄 파라미터들
+
 ) as dag:
     send_email_task=EmailOperator(
         task_id='send_email_task',
@@ -19,3 +17,4 @@ with DAG(
         subject='airflow 성공',
         html_content='Airflow작업 완료'
     )
+   
